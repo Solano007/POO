0948edbe2f99c87f468346b9be42dfc403978300
Emmanuel Solano <emmanuelcoc777@gmail.com>
2021-03-08 23:13:47 -0600

    maincra

--- src/proyecto/pkg1/practica2.java
+++ src/proyecto/pkg1/practica2.java
@@ -2,22 +2,23 @@ package proyecto.pkg1;
 
 import java.util.Scanner;
 
-public class Proyecto1 {
+public class practica2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 0;
-        System.out.println("¿cuantas lineas quieres? ");
-        
-        n=sc.nextInt();
-
+        int n;
+        System.out.print("¿Cuantas lineas quieres? ");
+        n = sc.nextInt();
         for (int i = 0; i < n; i++) {
+            for (int j = n-1; j > i; j--) {
+                System.out.print(" ");
+            }
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
-
+           System.out.println("");
         }
 
     }
+
 }
---
+++ src/proyecto/pkg1/practica3.java
@@ -0,0 +1,33 @@
+package proyecto.pkg1;
+
+import java.util.Scanner;
+
+public class practica3 {
+
+    public static int n;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String b[] = new String[n];
+        System.out.println("cuantos numeros vas a ingresar");
+        b[n]= " ";
+       
+       
+        System.out.println("dime tus numeros: ");
+        for (int i = 0; i < n; i++) {
+
+        
+        
+        
+        
+        
+        
+        
+        
+
+        }
+
+    }
+}
